c0b35d43b7a334556250c362c36bea5e1776b9ac
nis130 <nishitlimbani130@gmail.com>
2020-11-22 11:18:38 +0530

    major commit

---
+++ variables.py
@@ -0,0 +1,21 @@
+# GEN TILES CONFS
+
+# number of divisions per color (R, G and B)
+# DEPTH = 4 -> 4 * 4 * 4 = 64 colors
+DEPTH = 4
+# list of rotations, in degrees, to apply over the original image
+ROTATIONS = [0]
+
+
+#############################
+# Variables
+
+
+COLOR_DEPTH = 32
+RESIZING_SCALES = [0.5, 0.4, 0.3, 0.2, 0.1]
+PIXEL_SHIFT = None
+OVERLAP_TILES = False
+RENDER = False
+POOL_SIZE = 8
+IMAGE_TO_TILE = None
+TILES_FOLDER = None
